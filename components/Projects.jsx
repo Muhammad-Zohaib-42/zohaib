7765c0651295e771137f6c40aff578c7e605c266
Zohaib <ranjha5054@gmail.com>
2025-11-21 00:19:31 +0500

    add new project snake game to projects section

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -3,6 +3,7 @@ import Title from "./Title";
 import notesTaking from "../public/notes-taking-app.png"
 import restCountries from "../public/rest-countries-app.png"
 import expensesTracker from "../public/expenses-tracker-app.png"
+import snakeGame from '../public/snake-game.png'
 // import employeeManagement from "../public/employee-management-app.png"
 
 export default function Projects() {
@@ -12,9 +13,10 @@ export default function Projects() {
             <Title text="My Projects" />
             <h2 className="title">Let's <span className="hidden md:inline">Have a Look at</span> <span className="inline md:hidden">Explore</span> <span className="text-[#31F900] font-bold">My Portfolio</span></h2>
             <div className="mt-6 md:mt-10 grid grid-cols-[repeat(auto-fit,minmax(300px,1fr))] gap-6">
-                <ProjectBox img={notesTaking} skills={["HTML", "CSS", "JAVASCRIPT"]} title="Quick Notes App" url="https://quick-notes-app-by-constcoderr.netlify.app/" />
                 <ProjectBox img={restCountries} skills={["HTML", "CSS", "JAVASCRIPT"]} title="Rest Countries App" url="https://rest-countries-app-by-constcoderr.netlify.app/" />
                 <ProjectBox img={expensesTracker} skills={["REACT JS", "TAILWIND CSS"]} title="Expenses Tracker App" url="https://expenses-tracker-app-by-constcoderr.netlify.app/" />
+                <ProjectBox img={snakeGame} skills={["HTML", "CSS", "JAVASCRIPT"]} title="Snake Game" url="https://snake-game-omega-lac.vercel.app/" />
+                <ProjectBox img={notesTaking} skills={["HTML", "CSS", "JAVASCRIPT"]} title="Quick Notes App" url="https://quick-notes-app-by-constcoderr.netlify.app/" />
                 {/* <ProjectBox img={employeeManagement} skills={["NEXT JS", "TAILWIND CSS"]} title="Staff Manager App" url="https://employee-management-app-red-alpha.vercel.app/" /> */}
             </div>
         </div>
