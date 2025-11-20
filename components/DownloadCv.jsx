babef3ccc7471320d4070dd4f71a78861062ee49
Zohaib <ranjha5054@gmail.com>
2025-11-21 00:10:00 +0500

    i delete old cv and add updated cv

--- components/DownloadCv.jsx
+++ components/DownloadCv.jsx
@@ -4,6 +4,6 @@ import {motion} from "motion/react"
 
 export default function DownloadCv() {
   return (
-    <motion.a initial={{opacity: 0, scale: 0.8}} animate={{opacity: 1, scale: 1}} transition={{duration: 0.3, ease: "easeOut"}} href="/Muhammad-Zohaib-CV.pdf" download className="bg-black text-white btn border border-[#31F900] inline-block">Download CV</motion.a>
+    <motion.a initial={{opacity: 0, scale: 0.8}} animate={{opacity: 1, scale: 1}} transition={{duration: 0.3, ease: "easeOut"}} href="/Muhammad-Zohaib-Resume.pdf" download className="bg-black text-white btn border border-[#31F900] inline-block">Download CV</motion.a>
   )
 }
